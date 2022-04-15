560e794e60e84b54c9293e3d0f8a11ba0de2ba68
caglatopcu <98820226+topcucaglaa@users.noreply.github.com>
2022-04-15 11:20:07 +0300

    Online Ticari Otomasyon
    Web tabanlı bir ticari otomasyon aracı
    ASP.NET MVC5 çerçevesini kullanarak, c# backend kodları ile entity ve linq yaklaşımları kullanılarak bootstratrap css js eklentileri ile hazırlanmıştır.Veritabanı CodeFirst yaklaşımıyla yapıldı . Cari ve personel panelleri bulunmaktadır

---
+++ MvcOnlineTicariOtomasyon/MvcOnlineTicariOtomasyon/Migrations/202202171136579_mg2.cs
@@ -0,0 +1,20 @@
+﻿namespace MvcOnlineTicariOtomasyon.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class mg2 : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.mesajlars", "Gonderici", c => c.Int());
+            AlterColumn("dbo.mesajlars", "altkonu", c => c.Int());
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.mesajlars", "altkonu", c => c.String(maxLength: 200, unicode: false));
+            AlterColumn("dbo.mesajlars", "Gonderici", c => c.String(maxLength: 50, unicode: false));
+        }
+    }
+}
